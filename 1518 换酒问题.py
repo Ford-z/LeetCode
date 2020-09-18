7635285e8116e2aa2271622da8d9d2a34d0448a5
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-09-18 10:01:59 +0800

    2020/09/14

---
+++ 1518 换酒问题.py
@@ -0,0 +1,18 @@
+#小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。你购入了 numBottles 瓶酒。
+#如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
+#请你计算 最多 能喝到多少瓶酒。
+#来源：力扣（LeetCode）
+#链接：https://leetcode-cn.com/problems/water-bottles
+#著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        sum1=numBottles
+        empty=numBottles
+        drink=0
+        while(numExchange<=empty):
+            drink=int(empty/numExchange)
+            temp=empty%numExchange
+            empty=drink+temp
+            sum1+=drink
+        return sum1
