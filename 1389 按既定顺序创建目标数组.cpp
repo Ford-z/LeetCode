6f3609c1c4a4ea97e35c282acf0439938720c60f
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-09-18 21:44:22 +0800

    2020/09/18

---
+++ 1389 按既定顺序创建目标数组.cpp
@@ -0,0 +1,20 @@
+/*给你两个整数数组 nums 和 index。你需要按照以下规则创建目标数组：
+目标数组 target 最初为空。
+按从左到右的顺序依次读取 nums[i] 和 index[i]，在 target 数组中的下标 index[i] 处插入值 nums[i] 。
+重复上一步，直到在 nums 和 index 中都没有要读取的元素。
+请你返回目标数组。
+题目保证数字插入位置总是存在。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/create-target-array-in-the-given-order
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> ans;
+        for(int i=0;i<index.size();i++){
+            ans.insert(ans.begin()+index[i],nums[i]);
+        }
+        return ans;
+    }
+};
