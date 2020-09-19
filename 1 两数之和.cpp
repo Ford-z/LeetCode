17259c902c18ca88b5ebd41005b3fb471e8d1189
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-09-19 21:44:40 +0800

    2020/09/19

---
+++ 1 两数之和.cpp
@@ -0,0 +1,25 @@
+/*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> a;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                   a.push_back(i);
+                   a.push_back(j);
+                }
+               
+            }
+        }
+        return a;
+    }
+};
