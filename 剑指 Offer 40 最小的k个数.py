e46753bae5f185da02d17b40795cc13dc06e3b6b
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-09-29 11:22:02 +0800

    2020/09/29

---
+++ 剑指 Offer 40 最小的k个数.py
@@ -0,0 +1,5 @@
+#输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        arr.sort()
+        return arr[0:k]
