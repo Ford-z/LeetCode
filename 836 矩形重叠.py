f670f4dd9e38762798e1279431433c8412593fcb
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-10-03 15:51:24 +0800

    2020/10/03

---
+++ 836 矩形重叠.py
@@ -0,0 +1,13 @@
+#矩形以列表 [x1, y1, x2, y2] 的形式表示，其中 (x1, y1) 为左下角的坐标，(x2, y2) 是右上角的坐标。
+#如果相交的面积为正，则称两矩形重叠。需要明确的是，只在角或边接触的两个矩形不构成重叠。
+#给出两个矩形，判断它们是否重叠并返回结果。
+#来源：力扣（LeetCode）
+#链接：https://leetcode-cn.com/problems/rectangle-overlap
+#著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if(rec1[2]<=rec2[0] or rec2[2]<=rec1[0] or rec1[3]<=rec2[1] or rec2[3]<=rec1[1]):
+            return False
+        else:
+            return True
