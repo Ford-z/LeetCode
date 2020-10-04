3f77c485b2bb41e18dc38da30c45de4af946f2e2
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-10-04 12:17:35 +0800

    2020/10/04

---
+++ 1491 去掉最低工资和最高工资后的工资平均值.py
@@ -0,0 +1,13 @@
+#给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。
+#请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。
+#来源：力扣（LeetCode）
+#链接：https://leetcode-cn.com/problems/average-salary-excluding-the-minimum-and-maximum-salary
+#著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        salary.pop(0)
+        salary.pop(-1)
+        a=sum(salary)
+        ans=a/(len(salary))
+        return ans
