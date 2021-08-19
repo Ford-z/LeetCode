28ed9fea6793451704a7f8d00b0131c7285e1982
Ford-z <51626573+Ford-z@users.noreply.github.com>
2021-08-19 22:05:53 +0800

    2021/08/19

---
+++ 链表/203 移除链表元素.py
@@ -0,0 +1,15 @@
+#给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head==None:
+            return head
+        head.next=self.removeElements(head.next,val)
+        if head.val==val:
+            return head.next
+        else:
+            return head
