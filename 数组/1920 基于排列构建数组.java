9a77311869b28abd04e8dad6db39e2a790249d20
Ford-z <51626573+Ford-z@users.noreply.github.com>
2021-09-15 20:13:50 +0800

    2021/09/15

---
+++ 数组/1920 基于排列构建数组.java
@@ -0,0 +1,16 @@
+/**给你一个 从 0 开始的排列 nums（下标也从 0 开始）。请你构建一个 同样长度 的数组 ans ，其中，对于每个 i（0 <= i < nums.length），都满足 ans[i] = nums[nums[i]] 。返回构建好的数组 ans 。
+
+从 0 开始的排列 nums 是一个由 0 到 nums.length - 1（0 和 nums.length - 1 也包含在内）的不同整数组成的数组。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/build-array-from-permutation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*//
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
